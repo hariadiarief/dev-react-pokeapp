3f8561963f5637b83e00c0b04e6d3f31f2f2a447
Masagus Hariadi Arief <adi.arief77@gmail.com>
2020-11-16 23:34:39 +0700

    implement infinite-scroll

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
-	const [limit, setLimit] = useState(20)
+	const [limit, setLimit] = useState(10)
 	const [offset, setOffset] = useState(0)
-	const [pokemons, setPokemons] = useState({ isLoading: false, items: [] })
+	const [isHasMore, setIsHasMore] = useState(null)
+	const [pokemons, setPokemons] = useState({ isLoading: true, isLoadingMore: false, items: [] })
 	const [pokemonDetail, setPokemonDetail] = useState({ isLoading: true, items: [] })
 
 	useEffect(() => {
@@ -17,11 +19,12 @@ export default function Home() {
 
 		if (response.status === 200) {
 			const data = await response.json().then((response) => {
+				setIsHasMore(response.next ? true : false)
 				return response.results.map((result, index) => {
 					return { ...result, id: offset + index + 1 }
 				})
 			})
-			setPokemons((prevState) => ({ ...prevState, isLoading: false, items: data }))
+			setPokemons((prevState) => ({ ...prevState, isLoading: false, isLoadingMore: false, items: prevState.items.concat(data) }))
 			data.forEach((items) => fetchPokemonDetail(items.id))
 		} else {
 			console.error()
@@ -38,19 +41,29 @@ export default function Home() {
 		}
 	}
 
+	const loadMore = () => {
+		if (!pokemons.isLoading && !pokemons.isLoadingMore) {
+			setOffset(offset + limit)
+			setPokemons((prevState) => ({ ...prevState, isLoadingMore: true }))
+			fetchPokemon()
+		}
+	}
+
 	return (
 		<div className='home'>
 			<div className='home__title'>Poke Apps</div>
-			<div className='home__grid'>
-				{pokemons.items.map((pokemon, index) => {
-					return (
-						<Link className='home__grid__item' key={index} to={`/${pokemon.id}`}>
-							<img src={pokemonDetail.items.find((item) => item.id === pokemon.id)?.sprites.back_default} alt='' />
-							<span>{pokemon?.name}</span>
-						</Link>
-					)
-				})}
-			</div>
+			<InfiniteScroll pageStart={0} loadMore={loadMore} hasMore={isHasMore} loader={<h1>Loading</h1>}>
+				<div className='home__grid container'>
+					{pokemons.items.map((pokemon, index) => {
+						return (
+							<Link className='home__grid__item' key={index} to={`/${pokemon.id}`}>
+								<img src={pokemonDetail.items.find((item) => item.id === pokemon.id)?.sprites.back_default} alt='' />
+								<span>{pokemon?.name}</span>
+							</Link>
+						)
+					})}
+				</div>
+			</InfiniteScroll>
 		</div>
 	)
 }
